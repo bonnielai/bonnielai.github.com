42e0c730b373d0d113af543c5429ddf981d4c114
Bonnie <baoni_lai@163.com>
2020-04-06 17:25:47 +0800

    Init vuepress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  base: '/bonnielai.github.io/',
+  title: "bonnie's blog",
+  description: "This is bonnie's blog to write something about technology."
+}
+module.exports = {
+  base: '/bonnielai.github.io/',
+  title: "bonnie's blog",
+  description: "This is bonnie's blog to write something about technology.",
+  themeConfig: {
+      // 你的GitHub仓库，请正确填写
+      repo: 'https://bonnielai.github.io/',
+      // 自定义仓库链接文字。
+      repoLabel: 'My GitHub',
+      nav: [
+          { text: 'Home', link: '/' },
+          { text: 'FirstBlog', link: '/blog/20200406.md' }
+      ],
+      sidebar: [
+        ['/', '首页'],
+        ['/blog/20200406.md', '我的第一篇博客']
+      ]
+  }
+}
